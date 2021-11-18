aa0f85906f46a1520158873397b18aab73b23cae
lucasdmandrade <lucasdmandrade@gmail.com>
2021-11-18 14:01:34 -0300

    Inicio curso de C#(No vscode)

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharp_zero_ao_avancado.Fundamentos
+{
+    public class PrimeiroPrograma
+    {
+        public static void Executar(){
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+        }
+    }
+}
