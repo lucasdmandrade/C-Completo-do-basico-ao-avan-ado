2162e3f977f89bbf544a50755b4e7a0a62c43a28
lucasdmandrade <lucasdmandrade@gmail.com>
2021-11-22 11:31:43 -0300

    Retorno em cadeia

--- ClassesEMetodos/MetodosComRetorno.cs
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -16,6 +16,32 @@ namespace CSharp_zero_ao_avancado.ClassesEMetodos
                 return a / b;
             }
         }
+        class CalculadoraCadeia{
+            int memoria;
+
+            public CalculadoraCadeia Somar(int a){
+                memoria += a;
+                return this;
+            }
+
+            public CalculadoraCadeia Multiplicar(int a){
+                memoria *= a;
+                return this;
+            }
+
+            public CalculadoraCadeia Limpar(){
+                memoria = 0;
+                return this;
+            }
+
+            public CalculadoraCadeia Imprimir(){
+                System.Console.WriteLine(memoria);
+                return this;
+            }
+            public int Resultado() {
+                return memoria;
+            }
+        }
         public static void Executar(){
             var calculadoraComum = new CalculadoraComum();
             var resultado = calculadoraComum.Somar(5, 5);
@@ -24,6 +50,10 @@ namespace CSharp_zero_ao_avancado.ClassesEMetodos
             System.Console.WriteLine(calculadoraComum.Subtrair(2, 7));
             System.Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
             System.Console.WriteLine(calculadoraComum.Dividir(25, 8));
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir()
+                .Limpar().Imprimir();
         }
     }
 }
