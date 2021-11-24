8b26d058ec718f963c17f0f517acf745e95559c0
lucasdmandrade <lucasdmandrade@gmail.com>
2021-11-24 09:32:04 -0300

    Dictionary

--- Colecoes/ColecoesDictionary.cs
+++ Colecoes/ColecoesDictionary.cs
@@ -13,6 +13,22 @@ namespace CSharp_zero_ao_avancado.Colecoes
 
             if(filmes.ContainsKey(2001)) {
                 System.Console.WriteLine("2001 " + filmes[2001]);
+                System.Console.WriteLine("2001 " + filmes.GetValueOrDefault(2001));
+            }
+
+            System.Console.WriteLine(filmes.ContainsValue("Amnesia"));
+            System.Console.WriteLine($"Removeu {filmes.Remove(2004)}");
+            filmes.TryGetValue(2002, out string filme2002);
+            System.Console.WriteLine($"Filme 2002 {filme2002}");
+
+            foreach( var chave in filmes.Keys) {
+                System.Console.WriteLine(chave);
+            }
+            foreach ( var valor in filmes.Values) {
+                System.Console.WriteLine(valor);
+            }
+            foreach ( KeyValuePair< int, string > filme in filmes) {
+                System.Console.WriteLine($"{filme.Value} é de {filme.Key}");
             }
         }
     }
